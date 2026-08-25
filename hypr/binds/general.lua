ce626671fbf5e56f17a6158d3c2de29dce771d76
Frausty45 <frausty458@proton.me>
2026-08-25 20:16:23 +0200

    general binds added

---
+++ hypr/binds/general.lua
@@ -0,0 +1,33 @@
+local terminal = "kitty"
+
+hl.bind("SUPER + Q",
+    hl.dsp.exec_cmd(terminal)
+)
+
+hl.bind("SUPER + C",
+    hl.dsp.window.kill()
+)
+
+hl.bind("SUPER + F",
+    hl.dsp.window.fullscreen()
+)
+
+hl.bind("SUPER + V",
+    hl.dsp.window.float({ action = "toggle" })
+)
+
+hl.bind("SUPER + Left",
+    hl.dsp.focus({ direction = "l" })
+)
+
+hl.bind("SUPER + Down",
+    hl.dsp.focus({ direction = "d" })
+)
+
+hl.bind("SUPER + Up",
+    hl.dsp.focus({ direction = "u" })
+)
+
+hl.bind("SUPER + Right",
+    hl.dsp.focus({ direction = "r" })
+)

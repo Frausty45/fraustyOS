92be2ea099f1810a324679a3d9bcdbf2e33c6e64
Frausty45 <frausty458@proton.me>
2026-08-25 20:06:42 +0200

    input lua

---
+++ hypr/config/input.lua
@@ -0,0 +1,14 @@
+hl.config({
+    input = {
+        kb_layout = "hu",
+        follow_mouse = 2,
+        sensitivity = 0,
+
+        touchpad = {
+            natural_scroll = true,
+            tap_to_click = true,
+            disable_while_typing = true,
+            clickfinger_behavior = true,
+        },
+    },
+})

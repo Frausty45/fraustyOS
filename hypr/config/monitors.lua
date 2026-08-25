95ff720a7594acc82c9e0b99436a8de1b2de0582
Frausty45 <frausty458@proton.me>
2026-08-25 20:03:34 +0200

    monitors config for main monitor at home, and the backup monitor

--- hypr/config/monitors.lua
+++ hypr/config/monitors.lua
@@ -1,6 +1,20 @@
 hl.monitor({
-  output = "eDP-1",
-  mode = "1920x1080@144.06",
-  position = "0x0",
-  scale = 1,
+    output = "eDP-1",
+    mode = "1920x1080@144.06",
+    position = "0x0",
+    scale = 1,
+})
+
+hl.monitor({
+    output = "DP-1",
+    mode = "2560x1440@180",
+    position = "1920x0",
+    scale = 1,
+})
+
+hl.monitor({
+    output = "HDMI-1",
+    mode = "1680x1050@59.88",
+    position = "1920x0",
+    scale = 1,
 })

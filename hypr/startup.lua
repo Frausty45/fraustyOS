cfc946d7b83d18109a6a0d3c805eed3190d139ed
Frausty45 <frausty458@proton.me>
2026-08-27 23:33:50 +0200

    quickshell added to startup

--- hypr/startup.lua
+++ hypr/startup.lua
@@ -6,6 +6,5 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("wl-paste --type text --watch cliphist store")
     hl.exec_cmd("wl-paste --type image --watch cliphist store")
 
-    -- Later:
-    -- hl.exec_cmd("quickshell")
+    hl.exec_cmd("qs -p ~/Development/fraustyOS/quickshell/fraustyOS")
 end)
